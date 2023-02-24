d01a8a4a3063b9b76786319c76ecd5d09a5a1dd1
Alexmar Sanchez García <121136642+alexmarsanchez@users.noreply.github.com>
2023-02-24 14:35:19 +0100

    Create hello.py
    Primera clase con el curso de Harvard para EDX, mi primer Hola Mundo en Python

---
+++ hello.py
@@ -0,0 +1,19 @@
+print("Hola, Mundo")
+x = int(input("what's x? "))
+y = int(input("What's y? "))
+#if x<y:
+#    print("x is less than y")
+#elif x>y:
+#    print("x is greater than y")
+#else:
+#    print("x is equal to y")
+# another way to do this
+#if x < y or x > y:
+#  print("x is not equal to y")
+#else:
+#    print("x is equal to y")
+# Siempre la pregunta será ¿Cómo puedo hacer mi código más simple?
+if x == y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
